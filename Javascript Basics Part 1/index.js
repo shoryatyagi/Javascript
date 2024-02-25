03094f0b16bb72c2b9faff1675e824f5385cb615
shorya tyagi <shoryatyagi29@gmail.com>
2024-02-25 16:44:31 +0530

    file added

---
+++ Javascript Basics Part 1/index.js
@@ -0,0 +1,21 @@
+ //used to print on the console window
+console.log('hello shorya');
+alert('namaste shorya');
+
+let a = true;
+
+const num = 10;
+
+//camelCase should be used 
+
+//objects
+
+let person = {
+    firstname : 'John',age:34
+};
+//accessing using dot notation
+alert(person.firstname);
+
+//arrays
+let names =['love','babbar'];
+alert(names[0]);
